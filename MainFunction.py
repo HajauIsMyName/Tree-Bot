6e162ab71157b5e4481316f52cc563baf2803954
HajauIsMyName <hieuhau4@gmail.com>
2023-08-16 20:42:00 +0700

    update MainFuncion.py

--- MainFunction.py
+++ MainFunction.py
@@ -13,46 +13,41 @@ async def update_new_account(member: discord.Member) -> bool:
     with open(dataFile, "r") as file:
         try:
             users = json.load(file)
-            
+
         except FileNotFoundError:
-            pass
+            users = {
+                str(member.id): {
+                    "wallet": 0,
+                    "bank": 0,
+                    "isAdmin": False
+                }
+            }
 
     if str(member.id) not in users:
         users[str(member.id)] = {
             "wallet": 0,
             "bank": 0,
-            "isAdmin": False
-        }
-
-    with open(dataFile, "w") as file:
-        json.dump(users, file)
-
-    return True
-
-
-async def update_inventory(member: discord.Member) -> bool:
-    """
-    Add new user to inventory.json when not in this
-    """
-
-    users = {}
-    with open("inventory.json", "r") as file:
-        try:
-            users = json.load(file)
-
-        except FileNotFoundError:
-            pass
-
-        if str(member.id) not in users:
-            weapons = ["pistol", "cross_bow", "sniper"]
-            for weapon in weapons:
-                users[str(member.id)][weapon] = {
+            "isAdmin": False,
+            "inventory": {
+                "pistol": {
                     "ammo": 0,
                     "own": False,
                     "equip": False,
-                }
+                },
+                "cross_bow": {
+                    "ammo": 0,
+                    "own": False,
+                    "equip": False,
+                },
+                "sniper": {
+                    "ammo": 0,
+                    "own": False,
+                    "equip": False,
+                },
+            }
+        }
 
-    with open("inventory.json", "w") as file:
+    with open(dataFile, "w") as file:
         json.dump(users, file)
 
     return True
@@ -75,13 +70,29 @@ async def update_data(member: discord.Member, change: any = 0, mode: any = "wall
     users = await get_data()
     users[str(member.id)][mode] = change
 
-    with open(dataFile, "w") as f:
-        json.dump(users, f)
+    with open(dataFile, "w") as file:
+        json.dump(users, file)
 
 
-async def get_bank(member: discord.Member) -> dict:
+async def update_weapon(member: discord.Member, weapon: str = None, mode: str = "own", change: any = True) -> None:
+    """
+    Update inventory of user
+    """
+
+    users = await get_data()
+    users[str(member.id)]["inventory"][weapon][mode] = change
+
+    with open(dataFile, "w") as file:
+        json.dump(users, file)
+
+
+async def get_bank(member: discord.Member) -> tuple:
     """
     Return bank data of user
     """
     users = await get_data()
     return users[str(member.id)]["wallet"], users[str(member.id)]["bank"]
+
+async def get_inventory(member: discord.Member) -> tuple:
+    users = await get_data()
+    return users[str(member.id)]["inventory"]
